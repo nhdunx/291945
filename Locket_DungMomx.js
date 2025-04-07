7fd4c5141f79f5e61380383c8ef6c88e02f7fdf4
nhdunx <dungvipboy0@gmail.com>
2025-04-08 00:28:03 +0700

    Update Locket_DungMomx.js

--- Locket_DungMomx.js
+++ Locket_DungMomx.js
@@ -1,13 +1,11 @@
 const mapping = {
-  'Locket': ['Gold'], // Chắc chắn rằng đây là 'Gold' trong mapping
-  'iPad': ['Gold']
+  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
+  'Locket': ['Gold']
 };
 
-var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
-    obj = JSON.parse($response.body);
-
+var ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
+var obj = JSON.parse($response.body);
 obj.Attention = "UP TO NỖI HÀ DỤNG";
-
 var dunx = {
   is_sandbox: false,
   ownership_type: "PURCHASED",
@@ -16,15 +14,14 @@ var dunx = {
   expires_date: "2222-12-21T01:23:45Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "1945-09-02T14:00:00Z",
-  purchase_date: "1945-09-02T14:00:00Z",
+  original_purchase_date: "2025-03-28T01:23:45Z",
+  purchase_date: "2025-03-28T01:23:45Z",
   store: "app_store"
 };
-
 var titkok = {
   grace_period_expires_date: null,
-  purchase_date: "1945-09-02T14:00:00Z",
-  product_identifier: "Gold", // Đảm bảo rằng sản phẩm là "Gold"
+  purchase_date: "2025-03-28T01:23:45Z",
+  product_identifier: "com.dunx.premium.yearly",
   expires_date: "2222-12-21T01:23:45Z"
 };
 
@@ -38,23 +35,13 @@ if (match) {
   } else {
     obj.subscriber.subscriptions["com.dunx.premium.yearly"] = dunx;
   }
+  
+  // Đảm bảo cấp quyền cho Locket Gold
+  if (e === 'Gold') {
+    obj.subscriber.entitlements['Gold'] = titkok;
+  }
+  
   obj.subscriber.entitlements[e] = titkok;
-
-  obj.subscriber.features = [
-    "gold_badge", // Đây là huy hiệu "Locket Gold"
-    "premium_ui",
-    "priority_access",
-    "video_record_10s"
-  ];
-
-  obj.subscriber.video_recording_duration = 10;
-  obj.settings = obj.settings || {};
-  obj.settings.video_duration_limit = 10;
-
-  obj.subscriber.entitlements["Video10s"] = {
-    ...titkok,
-    is_video_10s_enabled: true
-  };
 } else {
   obj.subscriber.subscriptions["com.dunx.premium.yearly"] = dunx;
   obj.subscriber.entitlements.pro = titkok;
