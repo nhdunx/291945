47e4eb80d9cd0b1df5c91c07e599d5910dbd4ad8
nhdunx <dungvipboy0@gmail.com>
2025-04-08 00:21:22 +0700

    Update deleteHeader.js

--- deleteHeader.js
+++ deleteHeader.js
@@ -2,7 +2,7 @@
 > deleteHeader by DungMomx
 ***********************************************/	
 
-const version = 'V1.0.2';
+const version = 'V1.192.2';
 
 
 function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
